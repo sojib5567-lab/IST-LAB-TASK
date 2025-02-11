87b13aba1e5053442e90705395dfed548e2f557f
sojib5567-lab <sojib11359@gmail.com>
2025-02-11 14:46:49 +0600

    order

---
+++ Lab9_Task4.c
@@ -0,0 +1,12 @@
+// write a c program to print ASSCIi value  of correspending character
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character:");
+    scanf("%c",&ch);
+    printf("The ASSCII value of %c is %d \n",ch,ch);
+
+
+    return 0;
+}

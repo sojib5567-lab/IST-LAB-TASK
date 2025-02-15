84008ac45118e6fde0eacaa54652f2f9336467a0
sojib5567-lab <sojib11359@gmail.com>
2025-02-15 12:45:08 +0600

    Update Lab9_Task1.c

--- Lab9_Task1.c
+++ Lab9_Task1.c
@@ -1,17 +1,15 @@
-// write a c program to check weather a single character is vowel or consonent
+// find out vowel and consonent
 #include<stdio.h>
 int main(){
     char ch; printf("Enter:");
     scanf("%c",&ch);
-    if (ch =="A"|| ch=="a"|| ch=="E"|| ch=="e"|| ch=="I"|| ch=="i"|| ch=="O"||ch=="o"| ch=="U"|| ch=="u"   )
+    if (ch =='A'|| ch=='a'|| ch=='E'|| ch=='e'|| ch=='I'|| ch=='i'|| ch=='O'||ch=='o'|| ch=='U'|| ch=='u')
     {
        printf("It is vowel");
     }
     else{
            printf("It is a consonent");
     }
-
-
-
     return 0;
 }
+
